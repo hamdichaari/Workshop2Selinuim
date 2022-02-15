0a3fcf59f920048254132d91469188b3e787c1ca
extrainfo <extrainfo@DESKTOP-JJ8J2OK>
2022-02-15 22:57:14 +0100

    assertion la partie assert.equals
    fexture
    @befor et @ @after

--- src/test/java/www/amazon/gestionUtilisateur/AdminAuthentificationTestSuite.java
+++ src/test/java/www/amazon/gestionUtilisateur/AdminAuthentificationTestSuite.java
@@ -1,35 +1,54 @@
 package www.amazon.gestionUtilisateur;
 
 import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotEquals;
 
+//import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AdminAuthentificationTestSuite {
 	
 	//classe d'éxécusion des scénarios de teste ralatif a l"authentification
-	//
+
 	
 	//le nom de la class est le nom de la suite de test
 	
 	
 	
 	// le nom de chaque methode est e nom de chaque scénario de test
-	@Test
+	WebDriver driver ;
+	
+	@Before
+public void Unit () {
+	
+	
+ 
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	driver=	new ChromeDriver();
+	
+	}
+	
+	
+	
+	
 	public void AuthentificationDashbordAdminCorrect() {
 		
 		
 		
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+//System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		
 		String url="https://www.amazon.fr/" ;
 		
 		
-		WebDriver  driver = new ChromeDriver();    //Polymorphisme
+//	WebDriver  driver = new ChromeDriver();    //Polymorphisme
 		
 		driver.get(url);
 		driver.manage().window().maximize();
@@ -42,27 +61,52 @@ System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\
  driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("AdminProService");
  driver.findElement(By.xpath("//input[@id='signInSubmit']")).click();
  
+ 
+ 
+ 
+
  driver.findElement(By.xpath("//*[@id=\"nav-link-accountList-nav-line-1\"]")).click();
  //Thread.sleep(3000);
+ //implicite wait 
+ 
+ 
+
+
+ 
+//  String pass = driver.findElement(By.xpath("//span[@class='a-list-item']")).getText();
+
+//  assertEquals(pass,"The password is incorrect");
+ 
+ String element = driver.findElement(By.id("nav-link-accountList-nav-line-1")).getText();
+ assertEquals(element,"Bonjour admin");
+ 
+ 
+
+// String   Resultatattendu = "Bonjour admin" ;
+ 
+ 
+ /*
+ driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
  driver.findElement(By.id("twotabsearchtextbox")).sendKeys("sport" + Keys.ENTER );
 //Thread.sleep(3000);
+ driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 driver.close();
 		
-		
+		*/
 		
 		
 	}
 	
-	@Test
+@Test
 	public void TestLoginFieled()
 	{
 		
-System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\chromedriver_win32\\chromedriver.exe");
+//System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		
 		String url="https://www.amazon.fr/" ;
 		
 		
-		WebDriver  driver = new ChromeDriver();    //Polymorphisme
+	//	WebDriver  driver = new ChromeDriver();    //Polymorphisme
 		
 		driver.get(url);
 		driver.manage().window().maximize();
@@ -72,17 +116,37 @@ System.setProperty("webdriver.chrome.driver", "C:\\Users\\extrainfo\\Downloads\\
 // driver.findElement(By.xpath("//div[@id='nav-flyout-ya-signin']//span[@class='nav-action-inner'][normalize-space()='Identifiez-vous']")).click();
  driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("proservicetestauto@gmail.com");
  driver.findElement(By.xpath("//input[@id='continue']")).click();
- driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("AdminProSer");
+ driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("AdminProService");
  driver.findElement(By.xpath("//input[@id='signInSubmit']")).click();
+ 
+ 
+
+     
 	    
-	   String pass = driver.findElement(By.xpath("//span[@class='a-list-item']")).getText();
+	 //  String pass = driver.findElement(By.xpath("//span[@class='a-list-item']")).getText();
 	   
-	    assertEquals(pass,"The password is incorrect");
+	  //  assertEquals(pass,"The password is incorrect");
+ 
+ 
+ 
+ String element = driver.findElement(By.id("nav-link-accountList-nav-line-1")).getText();   
+ 
+ String   Resultaunattendu = "Bonjour User" ;
+ Assert.assertNotEquals(Resultaunattendu, element);
+
+	//   assertEquals(element,"Bonjour admin");
+	   
 		
 		
 	}
 	
+	@After
+public void exit () {
 	
-	
+	//	WebDriver  driver = new ChromeDriver();
+		
+	driver.close();
+	driver.quit();
+}
 
 }
